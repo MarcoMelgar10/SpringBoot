738200b64aa00230e983a65d61ecaf53e050c566
Melgar Parada Marco Antonio <nurmarcomelgar@gmail.com>
2025-06-10 10:53:45 -0400

    Spring_05 Understanding the ConfigurationProperties

--- spring_05/src/main/java/com/pizza/PizzaApplication.java
+++ spring_05/src/main/java/com/pizza/PizzaApplication.java
@@ -9,14 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Log
 @SpringBootApplication
 public class PizzaApplication implements CommandLineRunner {
+    private PizzaConfig pizzaConfig;
+    public PizzaApplication(PizzaConfig pizzaConfig){
+        this.pizzaConfig = pizzaConfig;
+    }
     public static void main(String[] args) {
         SpringApplication.run(PizzaApplication.class, args);
     }
     @Override
     public void run(final String... args){
-        final PizzaConfig pizzaConfig = new PizzaConfig(
-                "tomato", "mozzarella", "thin"
-        );
-        log.info(String.format("I want a %s crust pizza, with %s and %s sauce", pizzaConfig.getCrust(), pizzaConfig.getTopping(), pizzaConfig.getSouce()));
+        log.info(String.format("I want a %s crust pizza, with %s and %s sauce", pizzaConfig.getCrust(), pizzaConfig.getTopping(), pizzaConfig.getSauce()));
     }
 }
--- spring_05/src/main/java/com/pizza/config/PizzaConfig.java
+++ spring_05/src/main/java/com/pizza/config/PizzaConfig.java
@@ -3,12 +3,16 @@ package com.pizza.config;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@ConfigurationProperties(prefix ="pizza")
+@Configuration
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 public class PizzaConfig {
-    private String souce;
+    private String sauce;
     private String topping;
     private String crust;
 }
